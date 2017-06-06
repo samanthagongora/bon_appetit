e046e5c19dc1adb7c26ff748949846fb51665cc1
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-06 10:24:35 -0600

    adds shopping list print method and test

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -27,11 +27,11 @@ class Pantry
     converted_units = {}
     ingredients.each do |i, q|
       if q < 1
-        converted_units[i] = {quantity: (q * 1000), units: "Milli-Units"}
+        converted_units[i] = { quantity: (q * 1000), units: 'Milli-Units' }
       elsif q > 100
-        converted_units[i] = {quantity: (q / 100), units: "Centi-Units"}
+        converted_units[i] = { quantity: (q / 100), units: 'Centi-Units' }
       else
-        converted_units[i] = {quantity: q, units: "Universal Units"}
+        converted_units[i] = { quantity: q, units: 'Universal Units' }
       end
     end
     converted_units
@@ -47,5 +47,18 @@ class Pantry
         @shopping_list[i] += q
       end
     end
+  end
+
+  def print_shopping_list
+    human_readable = ""
+    items = @shopping_list.keys
+    items.each_with_index do |item, idx|
+      if items[idx + 1].nil?
+        human_readable << "* #{item}: #{@shopping_list[item]}"
+      else
+        human_readable << "* #{item}: #{@shopping_list[item]}\n"
+      end
+    end
+    human_readable
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -50,27 +50,27 @@ class PantryTest < Minitest::Test
   end
 
   def test_pantry_can_convert_units
-    r = Recipe.new("Spicy Cheese Pizza")
-    r.add_ingredient("Cayenne Pepper", 0.025)
-    r.add_ingredient("Cheese", 75)
-    r.add_ingredient("Flour", 500)
+    r = Recipe.new('Spicy Cheese Pizza')
+    r.add_ingredient('Cayenne Pepper', 0.025)
+    r.add_ingredient('Cheese', 75)
+    r.add_ingredient('Flour', 500)
 
     pantry = Pantry.new
 
     actual = pantry.convert_units(r)
     expected =
-    {"Cayenne Pepper" => {quantity: 25, units: "Milli-Units"},
-     "Cheese"         => {quantity: 75, units: "Universal Units"},
-     "Flour"          => {quantity: 5, units: "Centi-Units"}}
+      { 'Cayenne Pepper' => { quantity: 25, units: 'Milli-Units' },
+        'Cheese'         => { quantity: 75, units: 'Universal Units' },
+        'Flour'          => { quantity: 5, units: 'Centi-Units' } }
 
     assert_equal expected, actual
   end
 
   def test_it_can_have_empty_shopping_list
     pantry = Pantry.new
-    r = Recipe.new("Cheese Pizza")
-    r.add_ingredient("Cheese", 20)
-    r.add_ingredient("Flour", 20)
+    r = Recipe.new('Cheese Pizza')
+    r.add_ingredient('Cheese', 20)
+    r.add_ingredient('Flour', 20)
 
     actual = pantry.shopping_list
     expected = {}
@@ -80,44 +80,56 @@ class PantryTest < Minitest::Test
 
   def test_pantry_can_add_items_to_shopping_list
     pantry = Pantry.new
-    r = Recipe.new("Cheese Pizza")
-    r.add_ingredient("Cheese", 20)
-    r.add_ingredient("Flour", 20)
+    r = Recipe.new('Cheese Pizza')
+    r.add_ingredient('Cheese', 20)
+    r.add_ingredient('Flour', 20)
     pantry.add_to_shopping_list(r)
 
     actual = pantry.shopping_list
-    expected = {"Cheese" => 20,
-                "Flour" => 20}
+    expected = { 'Cheese' => 20,
+                 'Flour' => 20 }
 
     assert_equal expected, actual
   end
 
   def test_pantry_can_add_more_items_to_shopping_list
     pantry = Pantry.new
-    r = Recipe.new("Cheese Pizza")
-    r.add_ingredient("Cheese", 20)
-    r.add_ingredient("Flour", 20)
+    r = Recipe.new('Cheese Pizza')
+    r.add_ingredient('Cheese', 20)
+    r.add_ingredient('Flour', 20)
     pantry.add_to_shopping_list(r)
 
-    r_2 = Recipe.new("Spaghetti")
-    r_2.add_ingredient("Noodles", 10)
-    r_2.add_ingredient("Sauce", 10)
-    r_2.add_ingredient("Cheese", 5)
+    r_2 = Recipe.new('Spaghetti')
+    r_2.add_ingredient('Noodles', 10)
+    r_2.add_ingredient('Sauce', 10)
+    r_2.add_ingredient('Cheese', 5)
     pantry.add_to_shopping_list(r_2)
 
     actual = pantry.shopping_list
-    expected = {"Cheese" => 25,
-                "Flour" => 20,
-                "Noodles"=>10,
-                "Sauce"=>10}
+    expected = { 'Cheese' => 25,
+                 'Flour' => 20,
+                 'Noodles' => 10,
+                 'Sauce' => 10 }
 
     assert_equal expected, actual
   end
-# # Printing the shopping list
-# pantry.print_shopping_list
-# # * Cheese: 25
-# # * Flour: 20
-# # * Noodles: 10
-# # * Sauce: 10
-# # => "* Cheese: 20\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10"
+
+  def test_it_can_print_shopping_list
+    pantry = Pantry.new
+    r = Recipe.new('Cheese Pizza')
+    r.add_ingredient('Cheese', 20)
+    r.add_ingredient('Flour', 20)
+    pantry.add_to_shopping_list(r)
+
+    r_2 = Recipe.new('Spaghetti')
+    r_2.add_ingredient('Noodles', 10)
+    r_2.add_ingredient('Sauce', 10)
+    r_2.add_ingredient('Cheese', 5)
+    pantry.add_to_shopping_list(r_2)
+
+    actual = pantry.print_shopping_list
+    expected = "* Cheese: 25\n* Flour: 20\n* Noodles: 10\n* Sauce: 10"
+
+    assert_equal expected, actual
+  end
 end

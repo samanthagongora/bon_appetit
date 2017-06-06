387980290a2c48d11914fb286bf951ef103f019a
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-06 09:18:35 -0600

    adds empty stock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -8,4 +8,12 @@ class PantryTest < Minitest::Test
 
     assert_instance_of Pantry, new_pantry
   end
+
+  def test_stock_is_empty
+    new_pantry = Pantry.new
+    expected = {}
+    actual = new_pantry.stock
+
+    assert_equal expected, actual 
+  end
 end

ad28d812abb4f4d2b705ba930ccdfb60dc33527b
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-06 09:15:22 -0600

    adds pantry existence

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,9 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    new_pantry = Pantry.new
 
+    assert_instance_of Pantry, new_pantry
+  end
 end
